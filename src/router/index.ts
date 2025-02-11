9d7f2f30508476501708e7536d1878ab9056bef7
Maximiliano Luci <maximiliano.luci@icloud.com>
2025-02-11 14:49:17 +0100

    feat: move LogIn and SignUp

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,12 +12,22 @@ const router = createRouter({
     {
       path: "/log-in",
       name: "log-in",
-      component: () => import("@/modules/user/views/LogIn.vue"),
+      component: () => import("@/modules/auth/views/LogIn.vue"),
     },
     {
       path: "/sign-up",
       name: "sign-up",
-      component: () => import("@/modules/user/views/SignUp.vue"),
+      component: () => import("@/modules/auth/views/SignUp.vue"),
+    },
+    {
+      path: "/user",
+      children: [
+        {
+          path: "/:id",
+          name: "user-page",
+          component: () => import("@/modules/user/views/UserPage.vue"),
+        },
+      ],
     },
   ],
 });
