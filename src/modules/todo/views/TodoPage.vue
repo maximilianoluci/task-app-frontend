5d969449fca09a92822562ab342fe40082f7aa9b
Maximiliano Luci <maximiliano.luci@icloud.com>
2025-03-05 11:38:36 +0100

    feat: implement EditTodoComponent for improved todo editing experience; refactor TodoPage to utilize new component

--- src/modules/todo/views/TodoPage.vue
+++ src/modules/todo/views/TodoPage.vue
@@ -41,70 +41,42 @@
         <h1>Edit Todo</h1>
       </template>
       <template #body>
-        <div class="space-y-2">
-          <InputComponent v-model="editTodo.title" label="Title" />
-          <InputComponent v-model="editTodo.description" label="Description" />
-          <div class="flex gap-2">
-            <SelectComponent
-              class="w-1/2"
-              v-model="editTodo.priority"
-              :options="priorityOptions"
-              placeholder="Select a priority level"
-              label="Priority"
-            />
-            <DateTimeComponent class="w-1/2" v-model="editTodo.dueDate" label="Due Date" />
-          </div>
-          <CheckboxComponent v-model="editTodo.completed" bordered>Completed</CheckboxComponent>
-        </div>
-        <div class="flex justify-end gap-2">
-          <ButtonComponent color="secondary" @click="() => (isEditTodoModalVisible = false)"
-            >Cancel</ButtonComponent
-          >
-          <ButtonComponent
-            :disabled="loading"
-            icon="flowbite:floppy-disk-alt-outline"
-            @click="save"
-          >
-            {{ loading ? "Saving..." : "Save" }}
-          </ButtonComponent>
-        </div>
+        <EditTodoComponent
+          :edit-todo="editTodo"
+          :loading="loading"
+          @cancel="isEditTodoModalVisible = false"
+          @save="save"
+        />
       </template>
     </ModalComponent>
   </div>
 </template>
 
 <script setup lang="ts">
-import ButtonComponent from "@/components/button/ButtonComponent.vue";
-import CardComponent from "@/components/card/CardComponent.vue";
-import CheckboxComponent from "@/components/checkbox/CheckboxComponent.vue";
-import DateTimeComponent from "@/components/date-time/DateTimeComponent.vue";
-import InputComponent from "@/components/input/InputComponent.vue";
-import LoadingComponent from "@/components/loading/LoadingComponent.vue";
-import SelectComponent from "@/components/select/SelectComponent.vue";
-import TitleComponent from "@/components/title/TitleComponent.vue";
-import TodoService from "@/modules/todo/services/TodoService";
-import { Priority, type TodoId, type UpdateTodo } from "@/modules/todo/types/TodoTypes";
-import router from "@/router";
-import { formatDate } from "@/utils";
 import { defineAsyncComponent, onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
 import { z } from "zod";
 
+import ButtonComponent from "@/components/button/ButtonComponent.vue";
+import CardComponent from "@/components/card/CardComponent.vue";
+import LoadingComponent from "@/components/loading/LoadingComponent.vue";
+import TitleComponent from "@/components/title/TitleComponent.vue";
+import EditTodoComponent from "@/modules/todo/components/EditTodoComponent.vue";
+
+import TodoService from "@/modules/todo/services/TodoService";
+import { Priority, type TodoId, type UpdateTodo } from "@/modules/todo/types/TodoTypes";
+import router from "@/router";
+import { formatDate } from "@/utils";
+
 const ModalComponent = defineAsyncComponent(() => import("@/components/modal/ModalComponent.vue"));
 
 const route = useRoute();
-
 const todoService = TodoService.getInstance();
 
 const loading = ref<boolean>(false);
 const isEditTodoModalVisible = ref<boolean>(false);
 
 const todoId = route.params.id as string;
-
-const priorityOptions = Object.values(Priority).map((value) => ({
-  name: value.charAt(0).toUpperCase() + value.slice(1).toLowerCase(),
-  value,
-}));
 
 const todo = ref<TodoId | undefined>();
 const editTodo = ref<UpdateTodo | undefined>();
@@ -145,18 +117,18 @@ function handleBackButtonClick() {
   router.push({ name: "list-page", params: { id: todo.value.listId } });
 }
 
-async function save() {
-  if (!editTodo.value || !todo.value) {
+async function save(updatedTodo: UpdateTodo) {
+  if (!todo.value) {
     return;
   }
 
   loading.value = true;
 
   try {
-    editTodo.value.updatedAt = new Date();
+    updatedTodo.updatedAt = new Date();
 
-    const updatedTodo = await todoService.update(todoId, updateTodoSchema.parse(editTodo.value));
-    todo.value = updatedTodo;
+    const finalTodo = await todoService.update(todoId, updateTodoSchema.parse(updatedTodo));
+    todo.value = finalTodo;
     isEditTodoModalVisible.value = false;
   } catch (error) {
     console.error(error);
