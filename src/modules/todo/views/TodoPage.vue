4d4374d04f46fd9ca2826636afcf1a8570455e74
Maximiliano Luci <maximiliano.luci@icloud.com>
2025-03-05 11:54:13 +0100

    revert

--- src/modules/todo/views/TodoPage.vue
+++ src/modules/todo/views/TodoPage.vue
@@ -41,42 +41,70 @@
         <h1>Edit Todo</h1>
       </template>
       <template #body>
-        <EditTodoComponent
-          :edit-todo="editTodo"
-          :loading="loading"
-          @cancel="isEditTodoModalVisible = false"
-          @save="save"
-        />
+        <div class="space-y-2">
+          <InputComponent v-model="editTodo.title" label="Title" />
+          <InputComponent v-model="editTodo.description" label="Description" />
+          <div class="flex gap-2">
+            <SelectComponent
+              class="w-1/2"
+              v-model="editTodo.priority"
+              :options="priorityOptions"
+              placeholder="Select a priority level"
+              label="Priority"
+            />
+            <DateTimeComponent class="w-1/2" v-model="editTodo.dueDate" label="Due Date" />
+          </div>
+          <CheckboxComponent v-model="editTodo.completed" bordered>Completed</CheckboxComponent>
+        </div>
+        <div class="flex justify-end gap-2">
+          <ButtonComponent color="secondary" @click="() => (isEditTodoModalVisible = false)"
+            >Cancel</ButtonComponent
+          >
+          <ButtonComponent
+            :disabled="loading"
+            icon="flowbite:floppy-disk-alt-outline"
+            @click="save"
+          >
+            {{ loading ? "Saving..." : "Save" }}
+          </ButtonComponent>
+        </div>
       </template>
     </ModalComponent>
   </div>
 </template>
 
 <script setup lang="ts">
-import { defineAsyncComponent, onMounted, ref } from "vue";
-import { useRoute } from "vue-router";
-import { z } from "zod";
-
 import ButtonComponent from "@/components/button/ButtonComponent.vue";
 import CardComponent from "@/components/card/CardComponent.vue";
+import CheckboxComponent from "@/components/checkbox/CheckboxComponent.vue";
+import DateTimeComponent from "@/components/date-time/DateTimeComponent.vue";
+import InputComponent from "@/components/input/InputComponent.vue";
 import LoadingComponent from "@/components/loading/LoadingComponent.vue";
+import SelectComponent from "@/components/select/SelectComponent.vue";
 import TitleComponent from "@/components/title/TitleComponent.vue";
-import EditTodoComponent from "@/modules/todo/components/EditTodoComponent.vue";
-
 import TodoService from "@/modules/todo/services/TodoService";
 import { Priority, type TodoId, type UpdateTodo } from "@/modules/todo/types/TodoTypes";
 import router from "@/router";
 import { formatDate } from "@/utils";
+import { defineAsyncComponent, onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+import { z } from "zod";
 
 const ModalComponent = defineAsyncComponent(() => import("@/components/modal/ModalComponent.vue"));
 
 const route = useRoute();
+
 const todoService = TodoService.getInstance();
 
 const loading = ref<boolean>(false);
 const isEditTodoModalVisible = ref<boolean>(false);
 
 const todoId = route.params.id as string;
+
+const priorityOptions = Object.values(Priority).map((value) => ({
+  name: value.charAt(0).toUpperCase() + value.slice(1).toLowerCase(),
+  value,
+}));
 
 const todo = ref<TodoId | undefined>();
 const editTodo = ref<UpdateTodo | undefined>();
@@ -117,18 +145,18 @@ function handleBackButtonClick() {
   router.push({ name: "list-page", params: { id: todo.value.listId } });
 }
 
-async function save(updatedTodo: UpdateTodo) {
-  if (!todo.value) {
+async function save() {
+  if (!editTodo.value || !todo.value) {
     return;
   }
 
   loading.value = true;
 
   try {
-    updatedTodo.updatedAt = new Date();
+    editTodo.value.updatedAt = new Date();
 
-    const finalTodo = await todoService.update(todoId, updateTodoSchema.parse(updatedTodo));
-    todo.value = finalTodo;
+    const updatedTodo = await todoService.update(todoId, updateTodoSchema.parse(editTodo.value));
+    todo.value = updatedTodo;
     isEditTodoModalVisible.value = false;
   } catch (error) {
     console.error(error);
