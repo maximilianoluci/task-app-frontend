e5190f9cdaaa51ba41f7fbf136026e8ccdf710cf
Maximiliano Luci <maximiliano.luci@icloud.com>
2025-03-14 21:07:59 +0100

    refactor: add @nuxt/ui

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <RouterView />
+  <UApp>
+    <RouterView />
+  </UApp>
 </template>
 
 <script setup lang="ts">
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import "./assets/main.css";
 import { createPinia } from "pinia";
 import { createApp } from "vue";
 
+import ui from "@nuxt/ui/vue-plugin";
 import App from "./App.vue";
 import router from "./router";
 import { useAuthStore } from "./stores/auth";
@@ -12,6 +13,7 @@ const pinia = createPinia();
 
 app.use(pinia);
 app.use(router);
+app.use(ui);
 
 app.mount("#app");
 
