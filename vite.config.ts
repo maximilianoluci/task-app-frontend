a341b6f615e06bfd462202db7e71a865e8fdc826
Maximiliano Luci <maximiliano.luci@icloud.com>
2025-03-15 06:34:57 +0100

    refactor: update Tailwind CSS import and change primary color to cyan in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
           loading: "line-md:loading-twotone-loop",
         },
         colors: {
-          primary: "green",
+          primary: "cyan",
           neutral: "neutral",
         },
       },
